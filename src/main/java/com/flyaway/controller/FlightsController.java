09740a853bce751b90e41aff25c679f45993abfb
hirahul0011 <ra_10001@yahoo.co.in>
2023-09-13 13:59:29 +0530

    bookingDetailsCapture controller created

--- src/main/java/com/flyaway/controller/FlightsController.java
+++ src/main/java/com/flyaway/controller/FlightsController.java
@@ -2,6 +2,7 @@ package com.flyaway.controller;
 
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -12,10 +13,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.flyaway.dao.ParametersDAO;
 import com.flyaway.entity.Admin;
 import com.flyaway.entity.Flights;
+import com.flyaway.entity.Passengers;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 
@@ -169,6 +175,232 @@ public class FlightsController {
 			return page;		
 			
 		}
+
+		// Method I
+		//	@RequestMapping(value="bookingDetailsCapture",method=RequestMethod.POST)
+		//	public String bookingDetailsCapture(@RequestParam("firstname")String firstname,			
+		//			@Validated @NonNull @RequestParam(required = false,name="p1")String p1,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex1")String sex1,
+		//			@Validated @NonNull @RequestParam(required = false,name="age1")Integer age1,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p2")String p2,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex2")String sex2,
+		//			@Validated @NonNull @RequestParam(required = false,name="age2")Integer age2,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p3")String p3,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex3")String sex3,
+		//			@Validated @NonNull @RequestParam(required = false,name="age3")Integer age3,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p4")String p4,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex4")String sex4,
+		//			@Validated @NonNull @RequestParam(required = false,name="age4")Integer age4,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p5")String p5,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex5")String sex5,
+		//			@Validated @NonNull @RequestParam(required = false,name="age5")Integer age5,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p6")String p6,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex6")String sex6,
+		//			@Validated @NonNull @RequestParam(required = false,name="age6")Integer age6,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p7")String p7,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex7")String sex7,
+		//			@Validated @NonNull @RequestParam(required = false,name="age7")Integer age7,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p8")String p8,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex8")String sex8,
+		//			@Validated @NonNull @RequestParam(required = false,name="age8")Integer age8,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p9")String p9,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex9")String sex9,
+		//			@Validated @NonNull @RequestParam(required = false,name="age9")Integer age9,
+		//			
+		//			@Validated @NonNull @RequestParam(required = false,name="p10")String p10,
+		//			@Validated @NonNull @RequestParam(required = false,name="sex10")String sex10,
+		//			@Validated @NonNull @RequestParam(required = false,name="age10")Integer age10,
+		//			
+		//			@RequestParam("ID")Long ID,
+		//			@RequestParam("source")String source,
+		//			@RequestParam("destination")String destination,
+		//			@RequestParam("airline")String airline,			
+		//			@RequestParam("noOfPersons")Integer noOfPersons,
+		//			@RequestParam("billingAmount")Double billingAmount,
+		//			@RequestParam("travelDate")@DateTimeFormat(pattern="EEEEE MMMMM dd yyyy")Date travelDate,
+		//			ModelMap map) {
+		//				
+		////		for(int i=1;i<=noOfPersons;i++) {
+		////			String p=(String)request.getParameter("p"+i);
+		////		}
+		//		
+		//		String page="booking";
+		//		
+		//		map.addAttribute("firstname", firstname);
+		//		map.addAttribute("ID", ID);
+		//		map.addAttribute("source", source);
+		//		map.addAttribute("destination", destination);
+		//		map.addAttribute("airline", airline);
+		//		map.addAttribute("billingAmount", billingAmount);
+		//		map.addAttribute("noOfPersons", noOfPersons);
+		//		map.addAttribute("travelDate", travelDate);
+		//		
+		//		List<Passengers> passengerList=new ArrayList();
+		//		
+		//		if(p1!=null) {
+		//			Passengers passenger1=new Passengers(p1, sex1, age1);
+		//			passengerList.add(passenger1);
+		////			map.addAttribute("p1", p1);
+		////			map.addAttribute("sex1", sex1);
+		////			map.addAttribute("age1", age1);
+		//		}
+		//		
+		//		if(p2!=null) {
+		//			Passengers passenger2=new Passengers(p2, sex2, age2);
+		//			passengerList.add(passenger2);
+		////			map.addAttribute("p2", p2);
+		////			map.addAttribute("sex2", sex2);
+		////			map.addAttribute("age2", age2);
+		//		}
+		//		
+		//		if(p3!=null) {
+		//			Passengers passenger3=new Passengers(p3, sex3, age3);
+		//			passengerList.add(passenger3);
+		////			map.addAttribute("p3", p3);
+		////			map.addAttribute("sex3", sex3);
+		////			map.addAttribute("age3", age3);
+		//		}
+		//		
+		//		if(p4!=null) {
+		//			Passengers passenger4=new Passengers(p4, sex4, age4);
+		//			passengerList.add(passenger4);
+		////			map.addAttribute("p4", p4);
+		////			map.addAttribute("sex4", sex4);
+		////			map.addAttribute("age4", age4);
+		//		}
+		//		
+		//		if(p5!=null) {
+		//			Passengers passenger5=new Passengers(p5, sex5, age5);
+		//			passengerList.add(passenger5);
+		////			map.addAttribute("p5", p5);
+		////			map.addAttribute("sex5", sex5);
+		////			map.addAttribute("age5", age5);
+		//		}
+		//		
+		//		if(p6!=null) {
+		//			Passengers passenger6=new Passengers(p6, sex6, age6);
+		//			passengerList.add(passenger6);
+		////			map.addAttribute("p6", p6);
+		////			map.addAttribute("sex6", sex6);
+		////			map.addAttribute("age6", age6);
+		//		}
+		//		
+		//		if(p7!=null) {
+		//			Passengers passenger7=new Passengers(p7, sex7, age7);
+		//			passengerList.add(passenger7);
+		////			map.addAttribute("p7", p7);
+		////			map.addAttribute("sex7", sex7);
+		////			map.addAttribute("age7", age7);
+		//		}
+		//		
+		//		if(p8!=null) {
+		//			Passengers passenger8=new Passengers(p8, sex8, age8);
+		//			passengerList.add(passenger8);
+		////			map.addAttribute("p8", p8);
+		////			map.addAttribute("sex8", sex8);
+		////			map.addAttribute("age8", age8);
+		//		}
+		//		
+		//		if(p9!=null) {
+		//			Passengers passenger9=new Passengers(p9, sex9, age9);
+		//			passengerList.add(passenger9);
+		////			map.addAttribute("p9", p9);
+		////			map.addAttribute("sex9", sex9);
+		////			map.addAttribute("age9", age9);
+		//		}
+		//		
+		//		if(p10!=null) {
+		//			Passengers passenger10=new Passengers(p10, sex10, age10);
+		//			passengerList.add(passenger10);
+		////			map.addAttribute("p10", p10);
+		////			map.addAttribute("sex10", sex10);
+		////			map.addAttribute("age10", age10);
+		//		}
+		//		
+		//		map.addAttribute("passengerList", passengerList);
+		//		
+		//		return page;		
+		//		
+		//	}
+			
+			// Method II with HttpServletRequest
+			@RequestMapping(value="bookingDetailsCapture",method=RequestMethod.POST)
+			public String bookingDetailsCapture(@RequestParam("firstname")String firstname,
+					@RequestParam("ID")Long ID,
+					@RequestParam("source")String source,
+					@RequestParam("destination")String destination,
+					@RequestParam("airline")String airline,			
+					@RequestParam("noOfPersons")Integer noOfPersons,
+					@RequestParam("billingAmount")Double billingAmount,
+					@RequestParam("travelDate")@DateTimeFormat(pattern="EEEEE MMMMM dd yyyy")Date travelDate,
+					ModelMap map) {
+				
+				String page="booking";
+				List<Passengers> passengerList=new ArrayList();
+				String p;
+				String sex;
+				int age;
+				String errorMessages="";
+				boolean isNumber=true;
+				
+				//VVIMP
+				HttpServletRequest request=((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+				
+				for(int i=1;i<=noOfPersons;i++) {
+					if(request.getParameter("p"+i)!=null) {
+						if(request.getParameter("p"+i)=="") {
+							errorMessages=errorMessages+"<br>Please enter the Name of Passenger "+i+"! ";
+						}
+						
+						try {
+							int test=Integer.parseInt(request.getParameter("age"+i))/10;
+						} catch (NumberFormatException e) {
+							// TODO Auto-generated catch block
+							isNumber=false;
+						}
+						
+						if(!isNumber) {
+							errorMessages=errorMessages+"<br>Age should be a numeric digit for Passenger "+i+"! ";
+						}
+						
+						if(errorMessages!="") {
+							continue;
+						}else {
+							p=request.getParameter("p"+i);
+							sex=request.getParameter("sex"+i);
+							age=Integer.parseInt(request.getParameter("age"+i));
+							Passengers passenger=new Passengers(p, sex, age);
+							passengerList.add(passenger);					
+						}								
+					}
+				}
+				
+				if(errorMessages!="") {
+					map.addAttribute("errorMessages", errorMessages);
+					page="register";
+				}
+				
+				map.addAttribute("passengerList", passengerList);
+				
+				map.addAttribute("firstname", firstname);
+				map.addAttribute("ID", ID);
+				map.addAttribute("source", source);
+				map.addAttribute("destination", destination);
+				map.addAttribute("airline", airline);
+				map.addAttribute("billingAmount", billingAmount);
+				map.addAttribute("noOfPersons", noOfPersons);
+				map.addAttribute("travelDate", travelDate);			
+				
+				return page;		
+			}
 
 	
 
