0154e700b470d5056cf5bd664cde6d264b813308
hirahul0011 <ra_10001@yahoo.co.in>
2023-09-13 14:10:37 +0530

    hashCal method created to generate the transaction ID

--- src/main/java/com/flyaway/controller/FlightsController.java
+++ src/main/java/com/flyaway/controller/FlightsController.java
@@ -1,6 +1,8 @@
 package com.flyaway.controller;
 
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -623,6 +625,30 @@ public class FlightsController {
 					
 					return page;
 					
+				}
+
+				public String hashCal(String type,String str){
+					byte[] hashseq=str.getBytes();
+					StringBuffer hexString = new StringBuffer();
+					try{
+					MessageDigest algorithm = MessageDigest.getInstance(type);
+					algorithm.reset();
+					algorithm.update(hashseq);
+					byte messageDigest[] = algorithm.digest();
+				        
+					
+				
+					for (int i=0;i<messageDigest.length;i++) {
+						String hex=Integer.toHexString(0xFF & messageDigest[i]);
+						if(hex.length()==1) hexString.append("0");
+						hexString.append(hex);
+					}
+						
+					}catch(NoSuchAlgorithmException nsae){ }
+					
+					return hexString.toString();
+				
+				
 				}
 
 	
